2d1be4a055f670d5110d380d7f3706f3369f3cf0
Khanh Tran <ktran95@live.com>
2019-05-27 16:42:58 -0400

    implemented menu selection

--- app/src/main/java/com/example/flashcards/MainActivity.kt
+++ app/src/main/java/com/example/flashcards/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.example.flashcards
 
+import android.content.res.TypedArray
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
 import android.widget.Button
 import android.util.DisplayMetrics
+import android.view.Menu
+import android.view.MenuItem
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlin.collections.ArrayList
+import android.graphics.drawable.Drawable
+import com.bumptech.glide.RequestBuilder
+
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,13 +24,16 @@ class MainActivity : AppCompatActivity() {
         val metrics = DisplayMetrics()
         windowManager.defaultDisplay.getMetrics(metrics)
         paintView.init(metrics)
-        addListenerOnNextButton()
+//        GlideApp.with(this).load(R.raw.large_giphy_logo).into(giphyLogoView);
+//        val gifItemRequest = GlideApp.with(this).asDrawable()
         addListenerOnClearButton()
     }
 
 //    fun repopulateRandomList(): List<Int> {
 //
 //    }
+    private lateinit var displayArray: TypedArray
+    private var menu: Menu? = null
 
     private fun makeAndShuffleList(elements: Int): ArrayList<Int> {
         val list = ArrayList<Int>()
@@ -34,18 +44,16 @@ class MainActivity : AppCompatActivity() {
         return list
     }
 
-    private fun addListenerOnNextButton() {
-        val image: ImageView? = findViewById(R.id.hiraganaViewer)
+    private fun addListenerOnImageViewer() {
+        val image: ImageView? = findViewById(R.id.kanaViewer)
         val icons = ArrayList<Int>()
-
-        val arr = resources.obtainTypedArray(R.array.hiragana_characters)
-        (0 until arr.length()).forEach {
-            val icon = arr.getResourceId(it, -1)
+        (0 until displayArray.length()).forEach {
+            val icon = displayArray.getResourceId(it, -1)
             icons.add(icon)
         }
 
-        val elements = arr.length() - 1
-        arr.recycle()
+        val elements = displayArray.length() - 1
+        displayArray.recycle()
         var random = false
         var index = 0
         if (random) {
@@ -70,6 +78,7 @@ class MainActivity : AppCompatActivity() {
                     index = 0
                 }
                 image.setImageResource(icons[index])
+                paintView.clear()
             }
         }
     }
@@ -81,24 +90,76 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-//    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-//        val menuInflater = menuInflater
-//        menuInflater.inflate(R.menu.main, menu)
-//        return super.onCreateOptionsMenu(menu)
-//    }
+    fun displayHiraganaCharacters() {
+        displayArray = resources.obtainTypedArray(R.array.hiragana_characters)
+    }
+
+    fun displayHiraganaCharactersStrokes() {
+        displayArray = resources.obtainTypedArray(R.array.hiragana_characters_stroke)
+    }
+
+    fun displayHiraganaCharactersGifs() {
+        displayArray = resources.obtainTypedArray(R.array.hiragana_characters_stroke_gif)
+    }
+
+    fun displayKatakanaCharacters() {
+        displayArray = resources.obtainTypedArray(R.array.katakana_characters)
+    }
+
+    fun displayKatakanaCharactersStrokes() {
+        displayArray = resources.obtainTypedArray(R.array.katakana_characters_stroke)
+    }
+
+    fun displayKatakanaCharactersGifs() {
+        displayArray = resources.obtainTypedArray(R.array.katakana_characters_stroke_gif)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val menuInflater = menuInflater
+        menuInflater.inflate(R.menu.main, menu)
+        this.menu = menu
+        return super.onCreateOptionsMenu(menu)
+    }
+
+//    fun updateMenuTitles() {
+//        val bedMenuItem = menu?.findItem(R.id.hiraganaCharacter)
 //
-//    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-//        when (item.itemId) {
-//            R.id.normal -> {
-//                paintView.normal()
-//                return true
-//            }
-//            R.id.clear -> {
-//                paintView.clear()
-//                return true
-//            }
-//        }
-//
-//        return super.onOptionsItemSelected(item)
 //    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.hiraganaCharacter -> {
+                displayHiraganaCharacters()
+                addListenerOnImageViewer()
+                return true
+            }
+            R.id.hiraganaCharacterStrokes -> {
+                displayHiraganaCharactersStrokes()
+                addListenerOnImageViewer()
+                return true
+            }
+            R.id.hiraganaCharacterGifs -> {
+                displayHiraganaCharactersGifs()
+                addListenerOnImageViewer()
+                return true
+            }
+            R.id.katakanaCharacter -> {
+                displayKatakanaCharacters()
+                addListenerOnImageViewer()
+                return true
+            }
+            R.id.katakanaCharacterStrokes -> {
+                displayKatakanaCharactersStrokes()
+                addListenerOnImageViewer()
+                return true
+            }
+            R.id.katakanaCharacterGifs -> {
+                displayKatakanaCharactersGifs()
+                addListenerOnImageViewer()
+                return true
+            }
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
 }
